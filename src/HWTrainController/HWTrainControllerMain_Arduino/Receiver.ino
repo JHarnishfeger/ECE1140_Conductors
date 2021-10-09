3a757402d08e3e8976653fdb4fb182810e8d6348
evuke99 <evuke99@gmail.com>
2021-10-08 22:36:34 -0400

    f

---
+++ src/HWTrainController/HWTrainControllerMain_Arduino/Receiver.ino
@@ -0,0 +1,19 @@
+#define BAUD 9600
+#define DELAY_TIME 100
+#define led 32
+
+String receivedString;
+
+void setup() {
+  Serial.begin(BAUD);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0){
+    receivedString = Serial.readStringUntil('\n');
+  }
+
+  if (receivedString.equals("ON")) digitalWrite(led, HIGH);
+  else if (receivedString.equals("OFF")) digitalWrite(led, LOW);
+}
