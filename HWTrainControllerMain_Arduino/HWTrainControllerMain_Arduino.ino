cc2f48fc816c8beca44c6e2818ea095f96edd45d
evuke99 <65368200+evuke99@users.noreply.github.com>
2021-12-15 08:19:43 -0500

    Add files via upload

---
+++ HWTrainControllerMain_Arduino/HWTrainControllerMain_Arduino.ino
@@ -0,0 +1,90 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+//Reveiver and Joystick Variables
+String receivedString;
+String KpString = "1000";
+String KiString = "1000";
+String commandedSpeedString = "00.00";
+String currentSpeedString = "00.00";
+String suggestedSpeedString = "00.00";
+String speedLimitString = "00.00";
+String authorityString = "00.00";
+String stationCode = "00000";
+String stationName = "";
+String breakFailString = "0";
+String engineFailString = "0";
+String tcFailString = "0";
+
+//Power Variables
+double commandedPower;
+double Kp = 00.00;
+double Ki = 00.00;
+double commandedSpeed = 00.00;
+double currentSpeed = 00.00;
+double suggestedSpeed = 00.00;
+double speedLimit = 00.00;
+double authority = 99.99;
+double T = 1;
+double ek = 00.00;
+double ek1 = 00.00;
+double uk = 00.00;
+double uk1 = 00.00;
+double ek_SW = 00.00;
+double ek1_SW = 00.00;
+double uk_SW = 00.00;
+double uk1_SW = 00.00;
+
+//Pin Declarations
+const int leftDoorsPin = 24;
+const int rightDoorsPin = 26;
+const int interiorLightsPin = 28;
+const int exteriorLightsPin = 30;
+const int serviceBreakPin = 32;
+const int eBreakPin = 34;
+const int passengerBreakPin = 36;
+const int modePin = 38;
+
+//Transmitter Variables
+String parity = "1";
+String commandedPowerString;
+bool leftDoors = false;
+bool rightDoors = false;
+bool interiorLights = false;
+bool exteriorLights = false;
+bool serviceBreak = false;
+bool eBreak = false;
+bool passengerBreak = false;
+bool mode = true;
+
+void setup() {
+
+  //Pin Initializations
+  pinMode(leftDoorsPin, INPUT);
+  pinMode(rightDoorsPin, INPUT);
+  pinMode(interiorLightsPin, INPUT);
+  pinMode(exteriorLightsPin, INPUT);
+  pinMode(serviceBreakPin, INPUT);
+  pinMode(eBreakPin, INPUT);
+  pinMode(passengerBreakPin, INPUT);
+  pinMode(modePin, INPUT);
+  
+  Serial.begin(9600);
+
+  //Start the LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  
+  joystickSetup();
+}
+
+void loop() {
+  //put your main code here, to run repeatedly:
+  joystick();
+  ReceiveData();
+  calculatePower();
+  TransmitData();
+  delay(100);
+}
---
+++ HWTrainControllerMain_Arduino/Power.ino
@@ -0,0 +1,71 @@
+void calculatePower(){
+  
+  double avgPower;
+  double power;
+  
+  double power_redun;
+  double ek_redun = 0;
+  double ek1_redun = ek1;
+  double uk_redun = 0;
+  double uk1_redun = uk1;
+
+  //first power check
+  if(serviceBreak || eBreak || passengerBreak || breakFailString == "1" || authority <= 0){
+    power = 0;
+  }
+  else{
+    //if in automatic mode, use currentSpeed, if in manual use suggestedSpeed
+    if(mode && (currentSpeed < speedLimit)){
+      ek = suggestedSpeed - currentSpeed;
+    }
+    else if(!mode && (currentSpeed < speedLimit)){
+      ek = commandedSpeed - currentSpeed;
+    }
+    else{
+      ek = speedLimit - currentSpeed;
+    }
+
+    //Calculates uk and sets current error to previous error
+    uk = uk1 + (T/2) * (ek-ek1);
+    ek1 = ek;
+
+    //Calculates power
+    power = Kp * ek + Ki * uk;
+
+    if(power > 120000){
+      power = 120000;
+    }
+  }
+
+  //second power check (redundancy)
+  if(serviceBreak || eBreak || passengerBreak || breakFailString == "1" || authority <= 0){
+    power_redun = 0;
+  }
+  else{
+    //if in automatic mode, use currentSpeed, if in manual use suggestedSpeed
+    if(mode && (currentSpeed < speedLimit)){
+      ek_redun = suggestedSpeed - currentSpeed;
+    }
+    else if(!mode && (currentSpeed < speedLimit)){
+      ek_redun = commandedSpeed - currentSpeed;
+    }
+    else{
+      ek_redun = speedLimit - currentSpeed;
+    }
+
+    //Calculates uk and sets current error to previous error
+    uk_redun = uk1_redun + (T/2) * (ek-ek1);
+    ek1_redun = ek_redun;
+
+    //Calculates power
+    power_redun = Kp * ek_redun + Ki * uk_redun;
+
+    if(power_redun > 120000){
+      power_redun = 120000;
+    }
+  }
+
+  avgPower = (power + power_redun)/2;
+  commandedPower = avgPower;
+
+}
